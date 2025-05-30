cf410d575a356d8ddd10534d189696a65ca52012
Samir Meslem <accounts@samirmeslem.de>
2025-05-30 07:50:24 +0200

    Initial Commit

---
+++ FlowMode/FlowModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowModeApp.swift
+//  FlowMode
+//
+//  Created by Samir Meslem on 30.05.25.
+//
+
+import SwiftUI
+
+@main
+struct FlowModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
